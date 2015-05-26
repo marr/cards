e95c2c5f9dd1d7247db7c92f52925b18fe836e95
Dave Marr <dave@tilt.com>
2015-05-26 12:15:21 -0700

    updates

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,13 @@
 'use strict';
 import React from 'react';
 import Note from './Note';
+import CreditCardForm from './CreditCardForm';
 
 module.exports = React.createClass({
     render() {
         return <div>
             <Note name="Webpack" />
+            <CreditCardForm />
         </div>;
     }
 });
